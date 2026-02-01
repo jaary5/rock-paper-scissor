56e147ffbaaf8d95ae19533486db0e43ac921e8e
jaary5 <mujahid.jarif770@gmail.com>
2026-02-01 21:51:11 +0600

    not so good UI

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//function getPlayerChoice () {
+//    let playerChoice = prompt("rock, paper or scissor?");
+//    return playerChoice;
+//}
+
+let playerScore = 0;
+let computerScore = 0;
+
+// get reference of DOM elements
+const roundResult = document.querySelector('#round-result')
+const playerScoreSpan = document.querySelector('#player-score')
+const computerScoreSpan = document.querySelector('#computer-score')
+const winner = document.querySelector('#winner')
+const buttons = document.querySelectorAll('button')
+const resetButton = document.querySelector('#reset-btn')
+
+// hide the button until game is over
+resetButton.style.display = "none";
+
 function getComputerChoice () {
     let choice = Math.floor(Math.random() * 3);
 
@@ -10,51 +29,61 @@ function getComputerChoice () {
     }
 }
 
-function getHumanChoice () {
-    let choice = prompt("rock, paper or scissor?");
-    return choice;
+function playRound(playerSelection) {
+    // stop the game if someone wins
+    if (playerScore === 5 || computerScore === 5) return;
+
+    let computerSelection = getComputerChoice();
+    let resultMessage = "";
+
+    if (playerSelection === computerSelection) {
+        resultMessage = `It's a tie! Both chose ${playerSelection}`;
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissor") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissor" && computerSelection === "paper")) {
+            playerScore++;
+            resultMessage = `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        computerScore++;
+        resultMessage = `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+
+    // update UI
+    roundResult.textContent = resultMessage;
+    playerScoreSpan.textContent = playerScore;
+    computerScoreSpan.textContent = computerScore;
+
+    checkWinner();
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound () {
-    let h_Choice = getHumanChoice().toLowerCase();
-    let c_Choice = getComputerChoice();
-
-    if (h_Choice === "rock") {
-        if (c_Choice === "paper") {
-            computerScore++;
-        } else if (c_Choice === "rock") {
-
-        } else {
-            humanScore++;
-        }
-    } else if (h_Choice === "paper") {
-        if (c_Choice === "paper") {
-
-        } else if (c_Choice === "rock") {
-            humanScore++;
-        } else {
-            computerScore++;
-        }
-    } else {
-        if (c_Choice === "scissor") {
-            
-        } else if (c_Choice === "rock") {
-            computerScore++;
-        } else {
-            humanScore++;
-        }
+function checkWinner() {
+    if(playerScore === 5) {
+        winner.textContent = "Victory! You Win!";
+        resetButton.style.display = "block";
+    } else if (computerScore === 5) {
+        winner.textContent = "Gamer Over! You Lose!";
+        resetButton.style.display = "block";
     }
 }
 
-for (let i = 0; i < 5; i++) {
-    playRound();
-}   
+// Add event listener to buttons
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id);
+    });
+});
 
-if (humanScore === computerScore) {
-    console.log("Game Draw!");
-} else {
-    console.log((humanScore > computerScore) ? "You Win!" : "You Lose!");
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+
+    playerScoreSpan.textContent = "0";
+    computerScoreSpan.textContent = "0";
+    roundResult.textContent = "Play your move to start the game!";
+    winner.textContent = "";
+    resetButton.style.display = "none";
 }
+
+// Add event listener for play again
+resetButton.addEventListener('click', resetGame);
